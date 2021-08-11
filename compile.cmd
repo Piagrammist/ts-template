c251bf4025541f3a77cf1b682e04ba11785b4460
Piagrammist <Piagrammist@gmail.com>
2021-08-11 18:32:03 +0430

    Small fix on cmd compile script

--- compile.cmd
+++ compile.cmd
@@ -1,4 +1,4 @@
 @echo off
 
-start "Sass Compiler" /min cmd.exe /c "cd ""%~dp0"" && sass -w src/scss:public/css"
-start "Typescript Compiler" /min cmd.exe /c "cd ""%~dp0"" && tsc -w"
+start "Sass Compiler" /min cmd.exe /c "cd ""%~dp0"" && sass.cmd -w src/scss:public/css"
+start "Typescript Compiler" /min cmd.exe /c "cd ""%~dp0"" && tsc.cmd -w"
